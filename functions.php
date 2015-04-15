4afebc999d97044e486a8e8c47c301609836a22f
m-e-h <info@martyhelmick.com>
2015-04-15 12:02:58 -0400

    custom background color fix

--- functions.php
+++ functions.php
@@ -99,6 +99,7 @@ function bempress_includes() {
 	require_once $includes_dir . 'widgetize.php';
 	require_once $includes_dir . 'html-min.php';
     require_once $includes_dir . 'customizer/customizer.php';
+    require_once $includes_dir . 'customizer/custom-background.php';
 
     require_once $includes_dir . 'css-classes.php';
     new AttrTrumps();
