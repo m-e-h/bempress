6c6302be7b541f62632ae7d3470f6ecd65226360
m-e-h <info@martyhelmick.com>
2015-04-28 13:29:13 -0400

    fix some autoprefixer ie issues

--- inc/widgetize.php
+++ inc/widgetize.php
@@ -12,7 +12,7 @@ function bempress_register_sidebars() {
 	hybrid_register_sidebar( [
 		'id'          => 'primary',
 		'name'        => _x( 'Primary Sidebar', 'sidebar', 'bempress' ),
-		'before_widget'	=> '<section id="%1$s" class="widget widget-primary %2$s t-bg__2--light br u-p- u-p@md u-p+@lg u-mb- u-mb@md u-mb+@lg shadow--z1">',
+		'before_widget'	=> '<section id="%1$s" class="widget widget-primary %2$s t-bg__2--light br u-p@all u-mb- u-mb@md u-mb+@lg shadow--z1">',
 		'after_widget'	=> '</section>',
 		'before_title'  => '<h3 class="widget-title widget-primary__title">',
 		'after_title'	=> '</h3>',
@@ -21,7 +21,7 @@ function bempress_register_sidebars() {
 	hybrid_register_sidebar( [
 		'id'			=> 'footer',
 		'name'			=> _x( 'Footer Widgets', 'sidebar', 'bempress' ),
-		'before_widget' => '<section id="%1$s" class="widget widget-footer %2$s u-pr- u-pr@md u-pr+@lg u-mb- u-mb@md u-mb+@lg grid__item grid__item--flexed"><div class="br widget__wrap t-bg__1--dark">',
+		'before_widget' => '<section id="%1$s" class="widget widget-footer %2$s u-pr- u-pr@md u-pr+@lg u-mb- u-mb@md u-mb+@lg grid__item grid__item--flexed"><div class="br widget__wrap u-p@all t-bg__tint">',
 		'after_widget'	=> '</div></section>',
 		'before_title'  => '<h3 class="widget-title widget-footer__title">',
 		'after_title'   => '</h3>',
