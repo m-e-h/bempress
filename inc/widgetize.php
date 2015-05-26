ef72602ac387a8600efb8bddf6e53e321cd72c98
Marty Helmick <info@martyhelmick.com>
2015-05-26 00:02:24 -0400

    responsive class fix

--- inc/widgetize.php
+++ inc/widgetize.php
@@ -12,7 +12,7 @@ function bempress_register_sidebars() {
 	hybrid_register_sidebar( [
 		'id'          => 'primary',
 		'name'        => _x( 'Primary Sidebar', 'sidebar', 'bempress' ),
-		'before_widget'	=> '<section id="%1$s" class="widget widget-primary %2$s t-bg__white br u-p@all u-mb- u-mb@md u-mb+@lg shadow--z1">',
+		'before_widget'	=> '<section id="%1$s" class="widget widget-primary %2$s t-bg__white br u-p@respond u-mb@respond shadow--z1">',
 		'after_widget'	=> '</section>',
 		'before_title'  => '<h3 class="widget-title widget-primary__title">',
 		'after_title'	=> '</h3>',
@@ -21,7 +21,7 @@ function bempress_register_sidebars() {
 	hybrid_register_sidebar( [
 		'id'			=> 'footer',
 		'name'			=> _x( 'Footer Widgets', 'sidebar', 'bempress' ),
-		'before_widget' => '<section id="%1$s" class="widget widget-footer %2$s u-pr- u-pr@md u-pr+@lg u-mb- u-mb@md u-mb+@lg grid__item grid__item--flexed"><div class="br widget__wrap u-p@all t-bg__1">',
+		'before_widget' => '<section id="%1$s" class="widget widget-footer %2$s u-pr@respond u-mb@respond grid__item flex"><div class="br widget__wrap u-p@respond t-bg__1">',
 		'after_widget'	=> '</div></section>',
 		'before_title'  => '<h3 class="widget-title widget-footer__title">',
 		'after_title'   => '</h3>',
@@ -30,7 +30,7 @@ function bempress_register_sidebars() {
     hybrid_register_sidebar( [
         'id'            => 'panel-one',
         'name'          => _x( 'Panel One Widgets', 'doc' ),
-        'before_widget' => '<section id="%1$s" class="widget widget-panel %2$s u-pr- u-pr@md u-pr+@lg u-mb- u-mb@md u-mb+@lg grid__item grid__item--flexed"><div class="br widget__wrap u-p@all t-bg__1--glass shadow--z1">',
+        'before_widget' => '<section id="%1$s" class="widget widget-panel %2$s u-pr@respond u-mb@respond grid__item flex"><div class="br widget__wrap u-p@respond t-bg__1--glass shadow--z1">',
         'after_widget'  => '</div></section>',
         'before_title'  => '<h3 class="widget-title widget-panel__title">',
         'after_title'   => '</h3>',
