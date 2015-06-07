62952c0876a9a2ffa31d170f53cbbe93da04f13f
m-e-h <info@martyhelmick.com>
2015-06-07 15:24:05 -0400

    replace font awesome with material icons

--- inc/scripts.php
+++ inc/scripts.php
@@ -82,8 +82,8 @@ function bempress_enqueue_styles() {
     $suffix = hybrid_get_min_suffix();
 
     wp_enqueue_style(
-        'bem-font-awesome',
-        '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'
+        'bem-material-icons',
+        '//fonts.googleapis.com/icon?family=Material+Icons'
     );
 
 if ( is_child_theme() )
