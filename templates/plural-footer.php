dd51d9dc8c091f7d257db1bf9778c56b2cbf178d
m-e-h <info@martyhelmick.com>
2015-06-08 11:20:12 -0400

    profile format

--- templates/plural-footer.php
+++ templates/plural-footer.php
@@ -6,9 +6,9 @@
 
 <footer class="entry__footer">
     <p class="entry__meta wrap">
-        <?php flagship_entry_author(); ?>
-        <?php flagship_entry_published(); ?>
-        <?php flagship_entry_comments_link(); ?>
+        <span <?php hybrid_attr( 'entry-author' ); ?>><?php the_author_posts_link(); ?></span>
+        <time <?php hybrid_attr( 'entry-published' ); ?>><?php echo get_the_date(); ?></time>
+        <?php comments_popup_link( number_format_i18n( 0 ), number_format_i18n( 1 ), '%', 'comments-link', '' ); ?>
         <?php edit_post_link(); ?>
     </p><!-- .entry-meta -->
 </footer><!-- .entry-footer -->
--- templates/single-footer.php
+++ templates/single-footer.php
@@ -25,10 +25,10 @@
     endif; ?>
 
         <p class="entry__meta">
-            <?php flagship_entry_author(); ?>
-            <?php flagship_entry_published(); ?>
-            <?php flagship_entry_comments_link(); ?>
-            <?php edit_post_link(); ?>
+                <span <?php hybrid_attr( 'entry-author' ); ?>><?php the_author_posts_link(); ?></span>
+                <time <?php hybrid_attr( 'entry-published' ); ?>><?php echo get_the_date(); ?></time>
+                <?php comments_popup_link( number_format_i18n( 0 ), number_format_i18n( 1 ), '%', 'comments-link', '' ); ?>
+                <?php edit_post_link(); ?>
         </p><!-- .entry-meta -->
 
 	</footer><!-- .entry-footer -->
