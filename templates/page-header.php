711ca276d8ccbcf62474be7490754daadc079cdd
Marty Helmick <info@martyhelmick.com>
2015-06-16 11:51:23 -0400

    separate header from content

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,3 +1,16 @@
 <div class="page-header">
-  <h1><?= get_the_title(); ?></h1>
+    
+    <?php if ( ! hybrid_is_plural ) : // If not home, archive, search ?>
+    
+        <h1><?= get_the_title(); ?></h1>
+    
+    <?php else : ?>
+    
+	    <?php
+		the_archive_title( '<h1 class="page-title">', '</h1>' );
+		the_archive_description( '<div class="taxonomy-description">', '</div>' );
+	    ?>
+    
+    <?php endif; ?>
+    
 </div>
