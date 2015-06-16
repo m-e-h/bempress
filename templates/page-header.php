5582d26830cfdbf6c41f8fdad8ff34285a86786d
Marty Helmick <info@martyhelmick.com>
2015-06-16 12:05:13 -0400

    use titles

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,16 +1,5 @@
+<?php use Roots\Sage\Titles; ?>
+
 <div class="page-header">
-    
-    <?php if ( ! hybrid_is_plural ) : // If not home, archive, search ?>
-    
-        <h1><?= get_the_title(); ?></h1>
-    
-    <?php else : ?>
-    
-	    <?php
-		the_archive_title( '<h1 class="page-title">', '</h1>' );
-		the_archive_description( '<div class="taxonomy-description">', '</div>' );
-	    ?>
-    
-    <?php endif; ?>
-    
+    <h1><?= Titles\title(); ?></h1>
 </div>
