590cf3477b8403d88953fb79e0cc13eafd5eefbc
Marty Helmick <info@martyhelmick.com>
2015-06-16 12:25:19 -0400

    add content-none

--- content/content.php
+++ content/content.php
@@ -6,23 +6,21 @@ if ( is_singular( get_post_type() ) ) : ?>
 <?php 
 else : // If not viewing a single post. ?>
 
-    <?php if (!have_posts()) : ?>
+	<?php if ( have_posts() ) : ?>
 
-        <div class="alert alert-warning">
-            <?php _e('Sorry, no results were found.', 'bempress'); ?>
-        </div>
-    
-        <?php get_search_form(); ?>
-    
-    <?php endif; ?>
+		<?php while ( have_posts() ) : the_post(); ?>
 
-<?php while (have_posts()) : the_post(); ?>
+            <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
 
-    <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
+        <?php endwhile; ?>
 
-<?php endwhile; ?>
+        <?php the_posts_navigation(); ?>
 
-<?php the_posts_navigation(); ?>
+	<?php else : ?>
+
+		<?php get_template_part( 'templates/content', 'none' ); ?>
+		
+	<?php endif; // End check for posts. ?>
 
 <?php
 endif;
