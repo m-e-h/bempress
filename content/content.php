516da22a3f433c7f564d6994ba2ba031d657b2e2
Marty Helmick <info@martyhelmick.com>
2015-06-16 12:16:39 -0400

    remove archive header from content

--- content/content.php
+++ content/content.php
@@ -3,24 +3,18 @@ if ( is_singular( get_post_type() ) ) : ?>
 
     <?php get_template_part('templates/content-single', get_post_type()); ?>
 
-<?php else : // If not viewing a single post. ?>
+<?php 
+else : // If not viewing a single post. ?>
 
-    <header class="page-header">
-        <?php
-            the_archive_title( '<h1 class="page-title">', '</h1>' );
-            the_archive_description( '<div class="taxonomy-description">', '</div>' );
-        ?>
-    </header><!-- .page-header -->
+    <?php if (!have_posts()) : ?>
 
-<?php if (!have_posts()) : ?>
-
-    <div class="alert alert-warning">
-        <?php _e('Sorry, no results were found.', 'bempress'); ?>
-    </div>
-
-    <?php get_search_form(); ?>
-
-<?php endif; ?>
+        <div class="alert alert-warning">
+            <?php _e('Sorry, no results were found.', 'bempress'); ?>
+        </div>
+    
+        <?php get_search_form(); ?>
+    
+    <?php endif; ?>
 
 <?php while (have_posts()) : the_post(); ?>
 
