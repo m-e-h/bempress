14b4deacf4a2382f9a16c9805948503839a8c0ef
m-e-h <info@martyhelmick.com>
2015-06-16 13:23:27 -0400

    escape text

--- inc/titles.php
+++ inc/titles.php
@@ -6,16 +6,16 @@ namespace Roots\Sage\Titles;
  * Page titles
  */
 function title() {
-    if (is_archive()) { 
+    if (is_archive()) {
         return get_the_archive_title();
     }
     
     elseif (is_search()) {
-        return sprintf(__('Search Results for %s', 'bempress'), get_search_query());
+        return sprintf( esc_html__('Search Results for %s', 'bempress'), get_search_query());
     }
     
     elseif (is_404()) {
-        return __('Not Found', 'bempress');
+        return esc_html__('Not Found', 'bempress');
     }
     
     elseif (!hybrid_is_plural() && !is_404()) {
