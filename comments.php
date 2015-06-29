f66a008adb53efa905dbb30d0f02903035bcb9d6
m-e-h <info@martyhelmick.com>
2015-06-29 12:24:26 -0400

    style code

--- comments.php
+++ comments.php
@@ -1,38 +1,35 @@
 <?php
 /**
  * The template for displaying comments.
- *
- * @package BEMpress
  */
-
-if ( post_password_required() || ( !have_comments() && !comments_open() && !pings_open() ) ) {
+if (post_password_required() || (!have_comments() && !comments_open() && !pings_open())) {
     return;
 }
 ?>
 
     <?php tha_comments_before(); ?>
 
-    <section <?php hybrid_attr( 'comments-area' ); ?>>
+    <section <?php hybrid_attr('comments-area'); ?>>
 
-        <?php if ( have_comments() ) : ?>
+        <?php if (have_comments()) : ?>
 
             <h3 class="comments-number" id="comments-number"><?php comments_number(); ?></h3>
 
             <ol class="comment-list">
                 <?php
-                wp_list_comments( [
-                    'style'        => 'ol',
-                    'callback'     => 'hybrid_comments_callback',
+                wp_list_comments([
+                    'style' => 'ol',
+                    'callback' => 'hybrid_comments_callback',
                     'end-callback' => 'hybrid_comments_end_callback',
-                ] );
+                ]);
                 ?>
             </ol><!-- .comment-list -->
 
-            <?php get_template_part( 'comment/navigation' ); ?>
+            <?php get_template_part('comment/navigation'); ?>
 
-            <?php if ( ! comments_open() || ! pings_open() ) : ?>
+            <?php if (!comments_open() || !pings_open()) : ?>
 
-                <?php get_template_part( 'comment/error' ); ?>
+                <?php get_template_part('comment/error'); ?>
 
             <?php endif; ?>
 
