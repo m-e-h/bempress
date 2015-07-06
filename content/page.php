433f0bbddee19442c8b06fe78de72f7a549c6a0e
m-e-h <info@martyhelmick.com>
2015-07-06 19:33:44 -0400

    move css comb. remove extra wysiwyg

--- content/page.php
+++ content/page.php
@@ -6,8 +6,6 @@
           <?php the_content(); ?>
         </div>
 
-        <?php echo bempress_wysiwyg_output('bempress_extra_wysiwyg', get_the_ID()); ?>
-
         <footer <?php hybrid_attr('entry-footer'); ?>>
             <?php wp_link_pages(array(
                 'before' => '<nav class="page-nav"><p>'.__('Pages:', 'bempress'),
